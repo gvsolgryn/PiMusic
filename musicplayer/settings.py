4822d7af4120254e9d18eec04939cb913f56507b
gvsolgryn <tkdeh339@gmail.com>
2020-12-16 05:52:38 +0000

    mobile_web_update

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -127,9 +127,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
